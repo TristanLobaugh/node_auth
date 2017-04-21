6623f69dfe10abbabb445a7767309f81ed1d8f69
TristanLobaugh <ttrader_atl@yahoo.com>
2017-04-20 21:59:06 -0400

    update to es6

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Validator
 app.use(expressValidator({
-	errorFormatter: function(param, msg, value) {
+	errorFormatter(param, msg, value) {
 		const namespace = param.split('.');
 		const	root = namespace.shift();
 		let formParam = root;
 
 		while (namespace.length) {
-			formParam += '[' + namespace.shift() + ']';
+			formParam += `[${namespace.shift()}]`;
 		}
 		return {
 			param: formParam,
